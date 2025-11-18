9af55dda6f2b30c22c74f378feb57c776d95cec0
preetishtara <tarapreetish@gmail.com>
2025-11-18 11:41:22 +0530

    ./ server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoURI = "mongodb+srv://tarapreetish:abcd1234@cluster0.qynqj8e.mongodb.net/jay_db?appName=Cluster0";
+// const mongoURI = "mongodb+srv://tarapreetish:abcd1234@cluster0.qynqj8e.mongodb.net/jay_db?appName=Cluster0";
+
+const mongoURI = process.env.MONGODB_URI;
+
 
 mongoose.connect(mongoURI)
   .then(() => console.log("MongoDB connected"))
@@ -60,58 +63,3 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const path = require('path')
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const PORT = 7777
-
-// const app = express();
-
-// const mongoURI = "mongodb+srv://tarapreetish:abcd1234@cluster0.qynqj8e.mongodb.net/jay_db?appName=Cluster0";
-
-// mongoose.connect(mongoURI)
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.error("MongoDB connection error:", err));
-
-//   // Define Mongoose Schema and Model
-// const contactSchema = new mongoose.Schema({
-//   firstName: String,
-//   lastName: String,
-//   email: String,
-//   phone: String,
-// }, { timestamps: true });
-
-// const Contact = mongoose.model("Contact", contactSchema);
-
-// // POST endpoint to create new contact
-// app.post("/api/contacts", async (req, res) => {
-//   try {
-//     const { firstName, lastName, email, phone } = req.body;
-//     const newContact = new Contact({ firstName, lastName, email, phone });
-//     const savedContact = await newContact.save();
-//     res.status(201).json(savedContact);
-//   } catch (error) {
-//     console.error("Error saving contact:", error);
-//     res.status(500).json({ error: "Failed to save contact" });
-//   }
-// });
-
-
-
-// app.get("/", (req, res) => {
-//   res.send("hello world");
-// });
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
