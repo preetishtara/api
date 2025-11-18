31003775ae858cd2fe769b8a0865aa6736117abd
preetishtara <tarapreetish@gmail.com>
2025-11-18 13:57:13 +0530

    ./ server.js

--- server.js
+++ server.js
@@ -18,11 +18,28 @@ const app = express();
 // }));
 
 const cors = require('cors');
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://deft-jelly-27c5b3.netlify.app"
+];
+
 app.use(cors({
-  origin: 'https://deft-jelly-27c5b3.netlify.app',
+  origin: function(origin, callback) {
+    // allow requests with no origin like curl/postman
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
+
+app.options('*', cors());
 
 app.use(bodyParser.json());
 
