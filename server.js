eefc2ff4355d769ef2db46fd1a8f6156d7cb39cd
preetishtara <tarapreetish@gmail.com>
2025-11-18 11:50:13 +0530

    ./ server.js

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ const PORT = 7777;
 const app = express();
 
 // Middleware - IMPORTANT: Add these before your routes
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    // "https://your-frontend-domain.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 
-// const mongoURI = "mongodb+srv://tarapreetish:abcd1234@cluster0.qynqj8e.mongodb.net/jay_db?appName=Cluster0";
 
 const mongoURI = process.env.MONGODB_URI;
 
