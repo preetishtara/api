ac13a99140db649905a46cc71a2230779b54ff77
preetishtara <tarapreetish@gmail.com>
2025-11-24 03:46:36 +0530

    Initial commit

--- server.js
+++ server.js
@@ -16,41 +16,12 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://front-1-p7m9.onrender.com"  
+    "https://front-1-p7m9.onrender.com",
+    "https://www.elluzionai.com/"  
   ],
   credentials: true
 }));
 
-// app.use(cors({
-//   origin: [
-//     "http://localhost:3000",
-//     "https://deft-jelly-27c5b3.netlify.app"
-//   ],
-//   credentials: true
-// }));
-
-
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "https://deft-jelly-27c5b3.netlify.app"
-// ];
-
-// app.use(cors({
-//   origin: function(origin, callback) {
-//     // allow requests with no origin like curl/postman
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true,
-// }));
-
-// app.options('*', cors());
 
 app.use(bodyParser.json());
 
